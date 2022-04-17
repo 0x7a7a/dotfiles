a98f77bd2a1665314c67ae8b4f42e577a155b587
0x7a7a <298164449@qq.com>
2022-04-17 18:02:03 +0800

    update: update lsp-config

--- nvim/lua/lsp/typescript.lua
+++ nvim/lua/lsp/typescript.lua
@@ -4,7 +4,8 @@ require("lspconfig").tsserver.setup({
     capabilities = basic.capabilities,
 
     on_attach = function(client, bufnr)
-        basic.on_attach()
+        basic.on_attach(client, bufnr)
+
         client.resolved_capabilities.document_formatting = false
         client.resolved_capabilities.document_range_formatting = false
 
