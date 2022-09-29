416f90be0fa93bca408684a1a567137f9febdec4
0x7a7a <298164449@qq.com>
2022-09-29 10:02:03 +0800

    Add: add html-lsp

--- nvim/.config/nvim/lua/lsp/html.lua
+++ nvim/.config/nvim/lua/lsp/html.lua
@@ -1,7 +1,9 @@
 local basic = require 'lsp/basic'
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-require('lspconfig').tsserver.setup {
-  capabilities = basic.capabilities,
+require('lspconfig').html.setup {
+  capabilities = capabilities,
 
   on_attach = function(client, bufnr)
     basic.on_attach(client, bufnr)
@@ -9,13 +11,4 @@ require('lspconfig').tsserver.setup {
     client.resolved_capabilities.document_formatting = false
     client.resolved_capabilities.document_range_formatting = false
   end,
-
-  filetypes = {
-    'javascript',
-    'javascriptreact',
-    'javascript.jsx',
-    'typescript',
-    'typescriptreact',
-    'typescript.tsx',
-  },
 }
