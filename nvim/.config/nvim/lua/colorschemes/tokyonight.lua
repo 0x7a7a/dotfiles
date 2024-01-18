9f46f4bf96770634c39c8a3d04a046b750547885
0x7a7a <298164449@qq.com>
2024-01-18 15:43:47 +0800

    config kitty symbol_map

--- nvim/.config/nvim/lua/colorschemes/tokyonight.lua
+++ nvim/.config/nvim/lua/colorschemes/tokyonight.lua
@@ -8,7 +8,7 @@ return {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-      transparent = true, -- Enable this to disable setting the background color
+      transparent = false, -- Enable this to disable setting the background color
       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
       styles = {
         -- Style to be applied to different syntax groups
