b322f7704290ab6e312ceb5d7b961f852804e859
0x7a7a <298164449@qq.com>
2024-02-22 14:11:09 +0800

    Remove other session manager plugins

--- nvim/.config/nvim/lua/plugins/edit/auto-pairs.lua
+++ nvim/.config/nvim/lua/plugins/edit/auto-pairs.lua
@@ -1,18 +1,12 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  opts = {},
   config = function()
+    require('nvim-autopairs').setup({})
+
     -- If you want insert `(` after select function or method item
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     local cmp = require('cmp')
-    cmp.event:on(
-      'confirm_done',
-      cmp_autopairs.on_confirm_done({
-        map_char = {
-          tex = '',
-        },
-      })
-    )
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
   end,
 }
