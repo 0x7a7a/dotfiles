3d3702f8f1c91bc06787a82ea9f45cb1d2c5264b
0x7a7a <298164449@qq.com>
2024-02-29 05:22:18 +0800

    Add vim-fugitive keymapping and remove telescope git ignore

--- nvim/.config/nvim/lua/plugins/git/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/git/fugitive.lua
@@ -1,7 +1,11 @@
 return {
   'tpope/vim-fugitive',
   cmd = { 'G', 'Git' },
-  keys = {
-    { '<leader>G', '<cmd>G<cr>', desc = 'Fugitive' },
-  },
+  config = function()
+    Keymap('n', '<leader>G', '<cmd>G<cr>', { desc = 'Fugitive' })
+    Keymap('n', '<leader>gs', '<cmd>G status<cr>', { desc = 'Git status' })
+    Keymap('n', '<leader>ga', '<cmd>Gwrite<cr>', { desc = 'Git add' })
+    Keymap('n', '<leader>gc', '<cmd>G commit<cr>', { desc = 'Git commit' })
+    Keymap('n', '<leader>gp', '<cmd>G push<cr>', { desc = 'Git push' })
+  end,
 }
