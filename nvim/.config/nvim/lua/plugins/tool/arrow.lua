203829144097df6ce330db1602ddc08c1f11f0d7
0x7a7a <298164449@qq.com>
2024-03-10 01:22:00 +0800

    maps: change arrow index_keys

--- nvim/.config/nvim/lua/plugins/tool/arrow.lua
+++ nvim/.config/nvim/lua/plugins/tool/arrow.lua
@@ -1,17 +1,22 @@
 return {
   'otavioschwanck/arrow.nvim',
-  opts = {
-    show_icons = true,
-    leader_key = '<C-e>',
-  },
+  evet = 'VeryLazy',
   -- stylua: ignore
   keys = {
     { '<C-e>', function() require('arrow.persist').toggle() end },
-    { '<A-1>', function() require('arrow.persist').go_to(1) end },
-    { '<A-2>', function() require('arrow.persist').go_to(2) end },
-    { '<A-3>', function() require('arrow.persist').go_to(3) end },
-    { '<A-4>', function() require('arrow.persist').go_to(4) end },
     { '<A-h>', function() require('arrow.persist').previous() end },
     { '<A-l>', function() require('arrow.persist').next() end },
+    -- { '<A-1>', function() require('arrow.persist').go_to(1) end },
+    -- { '<A-2>', function() require('arrow.persist').go_to(2) end },
+    -- { '<A-3>', function() require('arrow.persist').go_to(3) end },
+    -- { '<A-4>', function() require('arrow.persist').go_to(4) end },
   },
+  config = function()
+    require('arrow').setup({
+      show_icons = true,
+      leader_key = '<C-e>',
+      -- index_keys = "123456789zxcbnmZXVBNM,afghjklAFGHJKLwrtyuiopWRTYUIOP",
+      index_keys = 'hjkl;',
+    })
+  end,
 }
