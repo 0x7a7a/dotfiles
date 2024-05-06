f0c7d1510c6c2930302d2294919807bc19959204
0x7a7a <298164449@qq.com>
2024-05-06 10:13:26 +0800

    chore: update reset.nvim dependencies version

--- nvim/.config/nvim/lua/plugins/tool/rest.lua
+++ nvim/.config/nvim/lua/plugins/tool/rest.lua
@@ -6,16 +6,18 @@ return {
   'rest-nvim/rest.nvim',
   ft = 'http',
   dependencies = {
-
-    'vhyrro/luarocks.nvim',
-    branch = 'go-away-python',
-    opts = {
-      rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' },
+    {
+      'vhyrro/luarocks.nvim',
+      priority = 1000,
+      config = true,
+      opts = {
+        rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' },
+      },
     },
-    config = function()
-      require('rest-nvim').setup()
-    end,
   },
+  config = function()
+    require('rest-nvim').setup()
+  end,
 }
 
 -- vim-test
