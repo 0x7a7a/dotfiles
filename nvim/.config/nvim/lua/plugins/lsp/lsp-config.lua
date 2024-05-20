000e098367b53a66a9d095939edc66dfa348dc95
0x7a7a <298164449@qq.com>
2024-05-20 16:11:53 +0800

    chore: use the new diagnostic configuration method

--- nvim/.config/nvim/lua/plugins/lsp/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp-config.lua
@@ -4,11 +4,21 @@ return {
   'neovim/nvim-lspconfig',
   event = 'BufEnter',
   config = function()
-    local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
-    for type, icon in pairs(signs) do
-      local hl = 'DiagnosticSign' .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
+    -- local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+    -- for type, icon in pairs(signs) do
+    --   local hl = 'DiagnosticSign' .. type
+    --   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    -- end
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = ' ',
+          [vim.diagnostic.severity.WARN] = ' ',
+          [vim.diagnostic.severity.HINT] = ' ',
+          [vim.diagnostic.severity.INFO] = ' ',
+        },
+      },
+    })
 
     local utils = require('utils')
     local lspconfig = require('lspconfig')
