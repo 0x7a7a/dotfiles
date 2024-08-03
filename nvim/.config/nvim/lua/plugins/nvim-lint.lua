7079ef824b87279ebe71dfda74a0ede07a73b7f4
0x7a7a <298164449@qq.com>
2024-08-03 18:26:48 +0800

    fix(nvim-lint): won't be enabled when eslint is not installed

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -1,6 +1,6 @@
 return {
   'mfussenegger/nvim-lint',
-  event = { 'BufReadPre', 'BufNewFile' },
+  event = 'VeryLazy',
   config = function()
     local lint = require('lint')
 
@@ -12,14 +12,15 @@ return {
       svelte = { 'eslint_d' },
     }
 
-    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-
-    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
+    if Z.npm_installed('eslint') then
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end
 
     vim.keymap.set('n', '<leader>lt', function()
       lint.try_lint()
