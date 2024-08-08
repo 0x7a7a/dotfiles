4615d11d1ca1f581e7e884f43d3c7f7c295c4946
0x7a7a <298164449@qq.com>
2024-08-08 11:48:18 +0800

    feat(conform): add enable/disable command in conform plugin

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -14,11 +14,14 @@ return {
         json = { 'prettierd' },
         svelte = { 'prettierd' },
       },
-      format_on_save = {
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
         -- I recommend these options. See :help conform.format for details.
-        lsp_fallback = true,
-        timeout_ms = 500,
-      },
+        return { timeout_ms = 500, lsp_format = 'fallback' }
+      end,
     })
 
     local function get_stylua_config()
@@ -44,5 +47,24 @@ return {
         timeout_ms = 500,
       })
     end, { desc = 'Format file (or range in visual mode)' })
+
+    -- Command to toggle format-on-save
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    })
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
   end,
 }
