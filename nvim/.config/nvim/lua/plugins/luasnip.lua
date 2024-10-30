5c307a203389c7d71421f7154186e24c5ed1d68a
0x7a7a <298164449@qq.com>
2024-10-31 03:22:05 +0800

    chore: add more luasnip doc

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -2,8 +2,11 @@ return {
   'L3MON4D3/LuaSnip',
   event = 'VeryLazy',
   config = function()
-    -- DOC https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#snipmate
-    -- Paths must start with'./lua'
+    -- Doc:
+    -- https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#snipmate
+    -- https://evesdropper.dev/files/luasnip/choice-dynamic/
+    -- https://www.dmsussman.org/resources/luasnippets/
+    -- Tips: Paths must start with'./lua'
     require('luasnip.loaders.from_lua').lazy_load({ paths = './lua/snippets' })
   end,
 }
