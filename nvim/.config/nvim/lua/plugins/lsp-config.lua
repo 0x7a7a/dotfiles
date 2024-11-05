3de19171029e7aa2d20f8e4e31b42fa7222d12ec
0x7a7a <298164449@qq.com>
2024-11-06 05:27:43 +0800

    chore: delete the unnecessary configuration of volar

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -154,27 +154,9 @@ return {
     })
 
     -- Vue language server
-    local function get_typescript_server_path(root_dir)
-      local global_ts = '/home/[yourusernamehere]/.npm/lib/node_modules/typescript/lib'
-      -- Alternative location if installed as root:
-      -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
-      local found_ts = ''
-      local function check_dir(path)
-        found_ts = util.path.join(path, 'node_modules', 'typescript', 'lib')
-        if util.path.exists(found_ts) then
-          return path
-        end
-      end
-      if util.search_ancestors(root_dir, check_dir) then
-        return found_ts
-      else
-        return global_ts
-      end
-    end
     lspconfig.volar.setup({
-      on_new_config = function(new_config, new_root_dir)
-        new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
-      end,
+      on_attach = custom_attach,
+      capabilities = capabilities,
     })
 
     -- https://github.com/williamboman/mason-lspconfig.nvim/issues/371#issuecomment-2018863753
@@ -194,6 +176,15 @@ return {
                 enableForWorkspaceTypeScriptVersions = true,
               },
             },
+          },
+          autoUseWorkspaceTsdk = true,
+          experimental = {
+            completion = {
+              enableServerSideFuzzyMatch = true,
+            },
+          },
+          typescript = {
+            updateImportsOnFileMove = { enabled = 'always' },
           },
         },
       },
