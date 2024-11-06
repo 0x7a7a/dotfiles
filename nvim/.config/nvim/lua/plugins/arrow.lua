6eff8f55120563c58e81629e80f7740dd4b3431b
0x7a7a <298164449@qq.com>
2024-11-07 04:36:50 +0800

    chore: remove ts-context and add useful diffview mapping

--- nvim/.config/nvim/lua/plugins/arrow.lua
+++ nvim/.config/nvim/lua/plugins/arrow.lua
@@ -1,5 +1,6 @@
 return {
   'otavioschwanck/arrow.nvim',
+  enabled = false,
   dependencies = {
     { 'echasnovski/mini.icons' },
   },
--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,4 +1,10 @@
+-- Cookbook: https://www.naseraleisa.com/posts/diff
 return {
   'sindrets/diffview.nvim',
   cmd = { 'DiffviewOpen' },
+  config = function()
+    vim.keymap.set('n', '<leader>hh', '<cmd>DiffviewFileHistory<cr>', { desc = 'Repo history' })
+    vim.keymap.set('n', '<leader>hf', '<cmd>DiffviewFileHistory --follow %<cr>', { desc = 'File history' })
+    vim.keymap.set('n', '<leader>hl', '<Cmd>.DiffviewFileHistory --follow<CR>', { desc = 'Line history' })
+  end,
 }
