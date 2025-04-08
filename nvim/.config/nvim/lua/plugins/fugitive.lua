8b1ea5afce8c3499f59de6a73f5631cf58e05791
0x7a7a <0x7a7a7@gmail.com>
2025-04-09 05:24:26 +0800

    chore(nvim): add vim-fugitive keymaps

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -3,8 +3,11 @@ return {
   event = 'VeryLazy',
   cmd = { 'G', 'Git' },
   keys = {
-    { '<leader>gg', '<cmd>G<CR>', desc = 'Fugitive: open Menu' },
-    { '<leader>gb', '<cmd>G blame<CR>', desc = 'Fugitive: git blame' },
-    { '<leader>gs', '<cmd>G status<CR>', desc = 'Fugitive: git status' },
+    { '<leader>ge', '<cmd>Gedit<CR>', desc = 'Gedit' },
+    { '<leader>gp', '<cmd>G pull<CR>', desc = 'Git pull' },
+    { '<leader>gl', '<cmd>Gclog<CR>', desc = 'Git log' },
+    { '<leader>gg', '<cmd>G<CR>', desc = 'Git menu' },
+    { '<leader>gb', '<cmd>G blame<CR>', desc = 'Git blame' },
+    { '<leader>gs', '<cmd>G status<CR>', desc = 'Git status' },
   },
 }
