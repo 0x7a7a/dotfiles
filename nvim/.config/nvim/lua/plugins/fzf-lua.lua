a6e88d9a40a6f6b581bc688ce7d6e56831d77283
0x7a7a <0x7a7a7@gmail.com>
2025-04-12 04:41:16 +0800

    chore(nvim): optimize fzf-lua keymaps

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -11,21 +11,19 @@ return {
     local files_opts = {
       previewer = false,
       cwd_prompt = false,
-      prompt = 'Files❯ ',
       winopts = {
         height = 0.5,
         width = 0.5,
       },
     }
 
-    vim.keymap.set('n', '<C-p>', function()
-      fzflua.files(files_opts)
-    end, { desc = '[S]earch [F]iles' })
+    -- stylua: ignore start
+    vim.keymap.set('n', '<C-p>', function() fzflua.files(files_opts) end, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<C-\\>', fzflua.buffers, { desc = 'Search buffers' })
+    vim.keymap.set('n', '<C-g>', fzflua.live_grep_native, { desc = '[S]earch by grep native' })
+    vim.keymap.set('n', '<Space>ss', fzflua.live_grep_resume, { desc = '[S]earch re[S]ume' })
     vim.keymap.set('n', '<Space>sc', fzflua.grep_curbuf, { desc = '[/] Fuzzily search in current buffer' })
     vim.keymap.set('n', '<Space>sa', fzflua.lsp_document_symbols, { desc = 'Search document symbols' })
-    vim.keymap.set('n', '<Space>sg', fzflua.live_grep_native, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<Space>ss', fzflua.live_grep_resume, { desc = '[S]earch re[S]ume' })
     vim.keymap.set('n', '<Space>sh', fzflua.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<Space>so', fzflua.oldfiles, { desc = '[S]earch [O]ldfiles' })
     vim.keymap.set('n', '<Space>sd', fzflua.diagnostics_workspace, { desc = '[S]earch [D]iagnostics' })
@@ -39,5 +37,6 @@ return {
     vim.keymap.set('n', 'gri', fzflua.lsp_implementations, { desc = '[G]oto [I]mplementation' })
     vim.keymap.set('n', 'gs', fzflua.lsp_document_symbols, { desc = '[G]oto Document [S]ymbols' })
     vim.keymap.set('n', 'grr', fzflua.lsp_references, { desc = '[G]oto [R]eferences' })
+    -- stylua: ignore end
   end,
 }
