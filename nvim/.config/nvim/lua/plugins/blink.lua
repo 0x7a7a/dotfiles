61ce16123719208df816e063814e67ad2110f446
0x7a7a <0x7a7a7@gmail.com>
2025-08-28 02:25:35 +0800

    feat: hide copilot if blink menu open

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -123,5 +123,18 @@ return {
         },
       },
     })
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'BlinkCmpMenuOpen',
+      callback = function()
+        vim.b.copilot_suggestion_hidden = true
+      end,
+    })
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'BlinkCmpMenuClose',
+      callback = function()
+        vim.b.copilot_suggestion_hidden = false
+      end,
+    })
   end,
 }
