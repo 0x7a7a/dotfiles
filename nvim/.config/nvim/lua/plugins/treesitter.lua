7fcbbea01717c5c5b4577bbdc1594f4641dc33d6
0x7a7a <0x7a7a7@gmail.com>
2025-09-24 21:04:11 +0800

    chore: update nvim-treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,63 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  -- dependencies = {
-  --   'nvim-treesitter/nvim-treesitter-textobjects',
-  -- },
-  event = 'BufEnter',
+  lazy = false,
+  branch = 'main',
   build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = {
-        'sql',
-        'zig',
-        'html',
-        'xml',
-        'css',
-        'javascript',
-        'typescript',
-        'tsx',
-        'go',
-        'vue',
-        'lua',
-        'http',
-        'json',
-        'vimdoc',
-        'make',
-      },
-
-      highlight = {
-        enable = true,
-        disable = function(_, buf)
-          local max_filesize = 1024 * 1024 -- 1MB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-      },
-
-      indent = {
-        enable = false,
-      },
-
-      -- textobjects = {
-      --   select = {
-      --     keymaps = {
-      --       ['aa'] = '@parameter.outer',
-      --       ['ia'] = '@parameter.inner',
-      --       ['af'] = '@function.outer',
-      --       ['if'] = '@function.inner',
-      --       ['ac'] = '@class.outer',
-      --       ['ic'] = '@class.inner',
-      --       ['ai'] = '@conditional.outer',
-      --       ['ii'] = '@conditional.inner',
-      --       ['ar'] = '@return.outer',
-      --       ['ir'] = '@return.inner',
-      --       ['al'] = '@loop.outer',
-      --       ['il'] = '@loop.inner',
-      --     },
-      --   },
-      -- },
-    })
-  end,
 }
