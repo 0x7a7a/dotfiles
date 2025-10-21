bd3cb6ea27e6ee6bbbbfc464a6350114c3dd73a9
0x7a7a <0x7a7a7@gmail.com>
2025-10-21 18:01:34 +0800

    feat: add treesitter-modules plugin

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,43 +1,49 @@
-local languages = {
-  'sql',
-  'zig',
-  'html',
-  'xml',
-  'css',
-  'javascript',
-  'typescript',
-  'tsx',
-  'go',
-  'vue',
-  'lua',
-  'http',
-  'json',
-  'vimdoc',
-  'make',
-}
-
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  branch = 'main',
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter').install(languages)
-
-    vim.api.nvim_create_autocmd('FileType', {
-      group = vim.api.nvim_create_augroup('treesitter.setup', {}),
-      callback = function(args)
-        local buf = args.buf
-        local filetype = args.match
-
-        local language = vim.treesitter.language.get_lang(filetype) or filetype
-        if not vim.treesitter.language.add(language) then
-          return
-        end
-
-        vim.treesitter.start(buf, language)
-        -- vim.bo[buf].syntax = 'ON'
-      end,
-    })
-  end,
+  'MeanderingProgrammer/treesitter-modules.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', branch = 'main', lazy = false },
+  opts = {
+    ensure_installed = {
+      'sql',
+      'zig',
+      'html',
+      'xml',
+      'css',
+      'javascript',
+      'typescript',
+      'tsx',
+      'go',
+      'vue',
+      'lua',
+      'http',
+      'json',
+      'vimdoc',
+      'make',
+    },
+    ignore_install = {},
+    sync_install = false,
+    -- automatically install missing parsers when entering buffer
+    auto_install = false,
+    fold = {
+      enable = false,
+      disable = false,
+    },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { 'vue' },
+    },
+    incremental_selection = {
+      enable = true,
+      -- set value to `false` to disable individual mapping
+      keymaps = {
+        init_selection = '<M-o>',
+        node_incremental = '<M-o>',
+        node_decremental = '<M-i>',
+        -- scope_incremental = '<M-o>',
+      },
+    },
+    indent = {
+      enable = false,
+      disable = false,
+    },
+  },
 }
