746d0007e1ed00a6c3ea6430f1fa858a49ddc0aa
0x7a7a <0x7a7a7@gmail.com>
2025-10-23 20:36:27 +0800

    chore(snippet): add makefile snippet

--- nvim/.config/nvim/lua/snippets/make.lua
+++ nvim/.config/nvim/lua/snippets/make.lua
@@ -1,5 +1,6 @@
 local ls = require('luasnip')
 local s = ls.snippet
+local i = ls.insert_node
 local fmt = require('luasnip.extras.fmt').fmt
 
 return {
@@ -13,6 +14,35 @@ return {
         -misc.clean_on_exit "true"
   ]],
       {}
+    )
+  ),
+
+  s(
+    { trig = 'gobuildlinux', desc = 'Go build linux amd64' },
+    fmt(
+      [[
+        build-linux:
+          GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o {} {}'
+      ]],
+      {
+        i(1, 'appname'),
+        i(2, 'main.go'),
+      }
+    )
+  ),
+
+  s(
+    { trig = 'sync', desc = 'rsync to remote server' },
+    fmt(
+      [[
+        sync:
+          rsync -avz --delete {} {}:{}
+      ]],
+      {
+        i(1, '/path/local'),
+        i(2, 'user@remote_server'),
+        i(3, '/path/remote'),
+      }
     )
   ),
 }
