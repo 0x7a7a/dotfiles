eea45a287d28a4a28ce419703527e9a9e0596d87
0x7a7a <0x7a7a7@gmail.com>
2025-11-21 21:13:36 +0800

    feat(nvim): add Makefile support with ftplugin and refactor snippets

--- nvim/.config/nvim/lua/snippets/make.lua
+++ nvim/.config/nvim/lua/snippets/make.lua
@@ -1,6 +1,7 @@
 local ls = require('luasnip')
 local s = ls.snippet
 local i = ls.insert_node
+local t = ls.text_node
 local fmt = require('luasnip.extras.fmt').fmt
 
 return {
@@ -8,41 +9,28 @@ return {
     'air',
     fmt(
       [[
-    live:
-        go run github.com/cosmtrek/air@latest \
-        -build.delay "100" \
-        -misc.clean_on_exit "true"
-  ]],
+live:
+	go run github.com/cosmtrek/air@latest \
+	-build.delay "100" \
+	-misc.clean_on_exit "true"
+    ]],
       {}
     )
   ),
 
-  s(
-    { trig = 'gobuildlinux', desc = 'Go build linux amd64' },
-    fmt(
-      [[
-        build-linux:
-          GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o {} {}'
-      ]],
-      {
-        i(1, 'appname'),
-        i(2, 'main.go'),
-      }
-    )
-  ),
+  s({ trig = 'gobuildlinux', desc = 'Go build linux amd64' }, {
+    t('build-linux:'),
+    t({ '', '\t' }),
+    t('GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o '),
+    i(1, 'appname'),
+  }),
 
   s(
     { trig = 'sync', desc = 'rsync to remote server' },
-    fmt(
-      [[
-        sync:
-          rsync -avz --delete {} {}:{}
-      ]],
-      {
-        i(1, '/path/local'),
-        i(2, 'user@remote_server'),
-        i(3, '/path/remote'),
-      }
-    )
+    fmt('sync:\n\trsync -avz --delete {} {}:{}', {
+      i(1, '/path/local'),
+      i(2, 'user@remote_server'),
+      i(3, '/path/remote'),
+    })
   ),
 }
