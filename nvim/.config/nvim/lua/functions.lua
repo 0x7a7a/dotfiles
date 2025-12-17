beab986e9ec0afe32ba4c1a37268e4ad56389a73
0x7a7a <0x7a7a7@gmail.com>
2025-12-17 14:26:05 +0800

    refactor(lsp): simplify LSP restart command using built-in functionality

--- nvim/.config/nvim/lua/functions.lua
+++ nvim/.config/nvim/lua/functions.lua
@@ -23,30 +23,6 @@ function Z.is_mac()
   return vim.fn.has('mac') == 1
 end
 
--- https://github.com/neovim/nvim-lspconfig/blob/03bc581e05e81d33808b42b2d7e76d70adb3b595/plugin/lspconfig.lua#L106C1-L127C5
-function Z.restart_all_lsp()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local clients = vim.lsp.get_clients({ bufnr = bufnr })
-
-  for _, c in ipairs(clients) do
-    local attached_buffers = vim.tbl_keys(c.attached_buffers) ---@type integer[]
-    local config = c.config
-    vim.lsp.stop_client(c.id, true)
-
-    vim.defer_fn(function()
-      local id = vim.lsp.start(config)
-      if id then
-        for _, b in ipairs(attached_buffers) do
-          vim.lsp.buf_attach_client(b, id)
-        end
-        vim.notify(string.format('Lsp `%s` has been restarted.', config.name))
-      else
-        vim.notify(string.format('Error restarting `%s`.', config.name), vim.log.levels.ERROR)
-      end
-    end, 600)
-  end
-end
-
 -- Add a custom command to copy the current file path to the clipboard
 local function copy_path(opts)
   local type = opts.fargs[1] or 'rel'
@@ -63,7 +39,9 @@ vim.api.nvim_create_user_command('CopyPath', copy_path, {
 })
 
 -- LSP commands
-vim.api.nvim_create_user_command('LspRestartAll', Z.restart_all_lsp, {})
+vim.api.nvim_create_user_command('LspRestartAll', function()
+  vim.cmd('lsp restart')
+end, {})
 vim.api.nvim_create_user_command('LspInfo', function()
   vim.cmd('checkhealth vim.lsp')
 end, {})
