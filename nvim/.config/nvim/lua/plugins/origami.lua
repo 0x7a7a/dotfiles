4290e0bdc12bc9c8a415a5024b4d8241c916f72a
0x7a7a <0x7a7a7@gmail.com>
2025-12-28 19:23:30 +0800

    chore(nvim): update origami config

--- nvim/.config/nvim/lua/plugins/origami.lua
+++ nvim/.config/nvim/lua/plugins/origami.lua
@@ -2,25 +2,30 @@ return {
   'chrisgrieser/nvim-origami',
   event = 'VeryLazy',
   opts = {
-    useLspFoldsWithTreesitterFallback = true,
+    useLspFoldsWithTreesitterFallback = {
+      enabled = true,
+      foldmethodIfNeitherIsAvailable = 'indent', ---@type string|fun(bufnr: number): string
+    },
     pauseFoldsOnSearch = true,
     foldtext = {
       enabled = true,
       padding = 3,
       lineCount = {
-        template = '%d lines',
+        template = '%d lines', -- `%d` is replaced with the number of folded lines
         hlgroup = 'Comment',
       },
-      diagnosticsCount = true,
-      gitsignsCount = true,
+      diagnosticsCount = true, -- uses hlgroups and icons from `vim.diagnostic.config().signs`
+      gitsignsCount = true, -- requires `gitsigns.nvim`
+      disableOnFt = { 'snacks_picker_input' }, ---@type string[]
     },
     autoFold = {
-      enabled = false,
+      enabled = true,
       kinds = { 'comment', 'imports' }, ---@type lsp.FoldingRangeKind[]
     },
     foldKeymaps = {
-      setup = true,
-      hOnlyOpensOnFirstColumn = false,
+      setup = true, -- modifies `h`, `l`, `^`, and `$`
+      closeOnlyOnFirstColumn = false, -- `h` and `^` only close in the 1st column
+      scrollLeftOnCaret = false, -- `^` should scroll left (basically mapped to `0^`)
     },
   },
   init = function()
