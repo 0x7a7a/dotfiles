2ef5c18a0686f98c72747e2afcc8a22ddb735ad8
0x7a7a <0x7a7a7@gmail.com>
2026-03-09 20:58:17 +0800

    chore: change 'an' keymap to lsp

--- nvim/.config/nvim/lua/plugins/mini-ai.lua
+++ nvim/.config/nvim/lua/plugins/mini-ai.lua
@@ -1,6 +1,24 @@
 return {
   'nvim-mini/mini.ai',
   config = function()
-    require('mini.ai').setup()
+    require('mini.ai').setup({
+      mappings = {
+        -- Main textobject prefixes
+        around = 'a',
+        inside = 'i',
+
+        -- Next/last variants
+        -- NOTE: These override built-in LSP selection mappings on Neovim>=0.12
+        -- Map LSP selection manually to use it (see `:h MiniAi.config`)
+        around_next = '',
+        inside_next = '',
+        around_last = 'al',
+        inside_last = 'il',
+
+        -- Move cursor to corresponding edge of `a` textobject
+        goto_left = 'g[',
+        goto_right = 'g]',
+      },
+    })
   end,
 }
